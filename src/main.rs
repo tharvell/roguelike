035c61ee8138c0a4e28fd24f904e4656fad746b6
tharvell <Thomas.Harvell@wexinc.com>
2019-05-29 20:47:50 -0400

    A screen with an @ on it

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use tcod::colors::*;
+use tcod::console::*;
+
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGHT: i32 = 50;
+
+const LIMIT_FPS: i32 = 20;
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut root: Root = Root::initializer()
+        .font("arial10x10.png", FontLayout::Tcod)
+        .font_type(FontType::Greyscale)
+        .size(SCREEN_WIDTH, SCREEN_HEIGHT)
+        .title("Rust/libtcod tutorial")
+        .init();    
+
+    tcod::system::set_fps(LIMIT_FPS);
+
+    while !root.window_closed() {
+        root.set_default_foreground(WHITE);
+        root.clear();
+        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.flush();
+        root.wait_for_keypress(true);
+     }     
 }
