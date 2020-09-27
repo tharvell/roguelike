c7bb89a0af32b0535a15fd1ee319c38a9f35b2fc
Tom Harvell <harvellt@gmail.com>
2020-09-27 00:26:46 -0400

    Working window with things on it

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,35 @@
-use rltk::{Rltk, GameState, Console};
+use rltk::{Rltk, GameState, Console, RGB, VirtualKeyCode};
+use specs::prelude::*;
+use std::cmp::{min, max};
+use specs_derive::Component;
 
 
-struct State {}
-impl GameState for State {
-    fn tick(&mut self, ctx : &mut Rltk) {
+#[derive(Component)]
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+#[derive(Component)]
+struct Renderable {
+    glyph: u8,
+    fg: RGB,
+    bg: RGB,
+}
+
+
+struct State {
+    ecs: World
+}
+impl GameState for State{
+    fn tick(&mut self, ctx: &mut Rltk){
         ctx.cls();
-        ctx.print(1,1, "Hello Rust World!!")
+        let positions = self.ecs.read_storage::<Position>();
+        let renderables = self.ecs.read_storage::<Renderable>();
+        
+        for (pos, render) in (&positions, &renderables).join() {
+            ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph)
+        }
     }
 }
 
@@ -14,6 +38,35 @@ fn main() {
     let context = RltkBuilder::simple80x50()
         .with_title("Rogue Stuff!!")
         .build();
-    let gs = State { };
+
+    let mut gs = State { 
+        ecs: World::new()
+      };
+    
+    gs.ecs.register::<Position>();
+    gs.ecs.register::<Renderable>();
+
+    gs.ecs
+        .create_entity()
+        .with(Position {x: 40, y:25 })
+        .with(Renderable{
+            glyph: rltk::to_cp437('@'),
+            fg: RGB::named(rltk::YELLOW),
+            bg: RGB::named(rltk::BLACK),
+        })
+        .build();
+    
+    for i in 0..10 {
+        gs.ecs
+          .create_entity()
+          .with(Position{x: i * 7, y: 20})
+          .with(Renderable{
+              glyph: rltk::to_cp437('@'),
+              fg: RGB::named(rltk::RED),
+              bg: RGB::named(rltk::BLACK),
+          })
+          .build();
+    }
+        
     rltk::main_loop(context, gs)
 }
